ac5c901951a1a32004f598b3ee420c5ec4601a5f
Vivek043 <83777814+Vivek043@users.noreply.github.com>
2025-10-01 14:10:01 -0500

    Delete app/app directory

--- app/app/model.py
+++
@@ -1,5 +0,0 @@
-def score_transaction(data: dict) -> float:
-    # Dummy logic: flag high amount transactions
-    amount = data.get("amount", 0)
-    risk_score = min(amount / 1000, 1.0)
-    return round(risk_score, 2)
