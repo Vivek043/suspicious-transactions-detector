c32e3449223a490c2ea1f2d784dc74b6315fe74d
Vivek043 <83777814+Vivek043@users.noreply.github.com>
2025-10-01 14:56:57 -0500

    Update model.py

--- app/model.py
+++ app/model.py
@@ -1,4 +1,4 @@
-def score_transaction(data: dict) -> float:
+'''def score_transaction(data: dict) -> float:
     amount = data.get("amount", 0)
 
     # If amount exceeds 10,000, assign max risk score
@@ -7,4 +7,16 @@ def score_transaction(data: dict) -> float:
 
     # Otherwise, scale risk score between 0 and 1
     risk_score = amount / 10000
-    return round(risk_score, 2)
+    return round(risk_score, 2)'''
+from scripts.features import extract_features
+
+def score_transaction(data: dict) -> float:
+    features = extract_features(data)
+
+    # Dummy model: weighted sum
+    score = (
+        0.6 * (features["amount"].values[0] / 10000) +
+        0.3 * features["location_risk"].values[0] +
+        0.1 * (features["hour"].values[0] / 24)
+    )
+    return round(min(score, 1.0), 2)
